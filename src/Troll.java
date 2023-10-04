f0c9640f86b2743b1492f6df28e0c3f1188beebd
pecoranera00 <pecoranera1929@gmail.com>
2023-10-04 23:27:10 +0200

    feat: added attribute rand

--- src/Troll.java
+++ src/Troll.java
@@ -1,6 +1,9 @@
+import java.util.Random;
+
 public class Troll {
     private String name;
     private int hitPoints;
     private int minAttack;
     private int maxAttack;
+    private Random rand;
 }
