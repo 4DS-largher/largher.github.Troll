98d29ea9b8c738ab613bd4ee2129ea4f6276f7ac
pecoranera00 <pecoranera1929@gmail.com>
2023-10-04 23:35:40 +0200

    test: method attack and getter

--- src/TrollTest.java
+++ src/TrollTest.java
@@ -3,5 +3,13 @@ public class TrollTest {
         Troll troll1 = new Troll("Grug", 100, 10, 20);
         Troll troll2 = new Troll("Franco", 120, 8, 18);
 
+        while (troll1.isAlive() && troll2.isAlive()) {
+            troll1.attack(troll2);
+            troll2.attack(troll1);
+
+            System.out.println(troll1.getName() + " has " + troll1.getHitPoints() + " hit points.");
+            System.out.println(troll2.getName() + " has " + troll2.getHitPoints() + " hit points.");
+            System.out.println();
+        }
     }
 }
