6defc52cbc9d39af9fe6355525ef9437f57b0664
pecoranera00 <pecoranera1929@gmail.com>
2023-10-04 23:33:28 +0200

    feat: added troll2 object

--- src/TrollTest.java
+++ src/TrollTest.java
@@ -1,6 +1,7 @@
 public class TrollTest {
     public static void main(String[] args) {
         Troll troll1 = new Troll("Grug", 100, 10, 20);
+        Troll troll2 = new Troll("Franco", 120, 8, 18);
 
     }
 }
